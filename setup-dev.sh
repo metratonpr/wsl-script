09af0891c96012100302d6ec891e72c1c5fbb3a4
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2025-05-24 13:11:56 -0300

    Update setup-dev.sh
    ajuste mb-string

--- setup-dev.sh
+++ setup-dev.sh
@@ -13,7 +13,9 @@ sudo apt install -y software-properties-common curl wget git unzip build-essenti
 # ================================
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
-sudo apt install -y php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-mbstring php8.2-xml php8.2-zip php8.2-mysql php8.2-bcmath php8.2-tokenizer php8.2-gd php8.2-intl php8.2-soap php8.2-opcache php8.2-readline php8.2-fpm php8.2-imap
+sudo apt install -y php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-mbstring php8.2-xml php8.2-zip php8.2-mysql php8.2-bcmath php8.2-tokenizer php8.2-gd php8.2-intl php8.2-soap php8.2-opcache php8.2-readline php8.2-fpm php8.2-imap sudo apt-get install php-mbstring
+sudo service apache2 restart  # ou sudo service php8.2-fpm restart se estiver usando FPM
+
 
 cd ~
 curl -sS https://getcomposer.org/installer | php
